3b52ccf1561fb49a6d4addf55bdf5f3851092ae4
BabySloth <wcao00@stuy.edu>
2018-02-08 21:58:44 -0500

    bb

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-def getCubesList(listLength: int) -> int:
+def getCubesList(listLength):
     cubesList = []
     for num in range(1, listLength + 1):
         cubesList = cubesList + [num ** 3]
     return cubesList
 
 
-def fibSequence(amount: int):
+def fibSequence(amount):
     x, y = 0, 1
     for i in range(0, amount):
         print(y)
         x, y = y, x + y
 
-def isPrimeRange (num1: int, num2: int):
+def isPrimeRange (num1, num2):
     for num in range(num1, num2 + 1):
         for test in range(2, num):
             if num % test == 0:
@@ -20,17 +20,27 @@ def isPrimeRange (num1: int, num2: int):
         else:
             print(str(num) + " is a prime number")
 
-#isPrimeRange(5, 10)
+isPrimeRange(100, 200)
 
-def factorization (number: int) -> list:
+def factorization (number = 100):
     factors = []
     for factor in range(1, number // 2):
         if number % factor == 0:
             factors.append(str(factor) + "x" + str(number / factor))
             #factors.extend([str(factor) + "x" + str(number / factor)])
     return factors
-
-print(factorization(100))
+#Doesn't work on negative numbers
+print(factorization(-100))
 
 def dontWantToCode():
     pass #Allows for empty class for completetion later
+
+def sumSquaresBetween(num1, num2):
+    sumOfNumbers = 0
+    for number in range(1, int(num2 ** .5) + 1):
+        squared = number ** 2
+        if squared >= num1 and squared <= num2:
+            sumOfNumbers = sumOfNumbers + squared
+    return sumOfNumbers
+
+print(sumSquaresBetween(5, 10))
