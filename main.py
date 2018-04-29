07c410c645700a12053565b866edefd3553c5de5
BabySloth <wcao00@stuy.edu>
2018-04-29 17:23:37 -0400

    question3

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ def counts(datalists, targets):
         for item in list:
             if item in targets:
                 counter[-1] += 1
-
     return counter
+
 
 datalists = [[4000, 1100, 3600, 1000], [12, -10000], [13, 0, 13], []]
 targets = [0, 12, -7, 13]
 
+print("q2")
 print(counts(datalists, targets))
+
+def countdown_by_n(f, b):
+    if f - b < 0:
+        print(f)
+    else:
+        print(f)
+        return countdown_by_n(f - b, b)
+
+print("\n\nq3:")
+print(countdown_by_n(21, 7))
