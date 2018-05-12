aa9b0dce1064ddfcf07071f50970f8885ba5bc5c
BabySloth <wcao00@stuy.edu>
2018-05-11 21:19:22 -0400

    homework update

--- format.py
+++ format.py
@@ -1,38 +1,41 @@
 def the_jungle():
-	# Ask the user for information
-	a = raw_input("Give me an animal \n")	
-	f = raw_input("Give me a food \n")
-	c = raw_input("Give me a city \n")
-	
-	text = open("jungle.txt", "r")
-	
-	o = text.read()	
-	o = o.format(animal = a, food = f, city = c)
+    # Ask the user for information
+    a = input("Give me an animal \n")
+    f = input("Give me a food \n")
+    c = input("Give me a city \n")
 
-	write = open("jC.txt", 'w')
-	write.write(o)
+    text = open("jungle.txt", "r")
 
-	write.close()
-	text.close()
-	
+    o = text.read()
+    o = o.format(animal=a, food=f, city=c)
+
+    write = open("jC.txt", 'w')
+    write.write(o)
+
+    write.close()
+    text.close()
+
+
 def the_newspaper():
-	adj = raw_input("Give an adjective \n")
-	n = raw_input("Give a noun \n")
-	v = raw_input("Give a verb \n")
-	c = raw_input("Give a color \n")
-	e = raw_input("Give an exclamation \n")
-	a = raw_input("Give an animal \n")
+    adj = input("Give an adjective \n")
+    n = input("Give a noun \n")
+    v = input("Give a verb \n")
+    c = input("Give a color \n")
+    e = input("Give an exclamation \n")
+    a = input("Give an animal \n")
 
-	text = open("newspaper.txt", "r")
-	
-	original = text.read()
-	original = original.format(ADJECTIVE = adj, NOUN = n, VERB = v, COLOR = c, EXCLAMATION = e, ANIMAL = a)
+    text = open("newspaper.txt", "r")
 
-	write = open("newC.txt", "w")
-	write.write(original)
+    original = text.read()
+    original = original.format(ADJECTIVE=adj, NOUN=n, VERB=v, COLOR=c, EXCLAMATION=e, ANIMAL=a)
 
-	write.close()
-	text.close()
+    write = open("newC.txt", "w")
+    write.write(original)
+
+    write.close()
+    text.close()
+
 
 if __name__ == "__main__":
+	the_jungle()
 	the_newspaper()
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-def counts(datalists, targets):
-    counter = []
-
-    for list in datalists:
-        counter.append(0)
-        for item in list:
-            if item in targets:
-                counter[-1] += 1
-    return counter
-
-
-datalists = [[4000, 1100, 3600, 1000], [12, -10000], [13, 0, 13], []]
-targets = [0, 12, -7, 13]
-
-print("q2")
-print(counts(datalists, targets))
-
-def countdown_by_n(f, b):
-    if f - b < 0:
-        print(f)
+def get_all_prereqs():
+    req = []
+    if len(self.prereqs) == 0:
+        return []
     else:
-        print(f)
-        return countdown_by_n(f - b, b)
-
-print("\n\nq3:")
-print(countdown_by_n(21, 7))
+        for course in req:
+            req.append(course.get_all_prereqs())
+        
