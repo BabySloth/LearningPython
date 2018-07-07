d49cd6ef4791676ef18ac8b04e14ed36c81a2ec8
William <williamcao999@gmail.com>
2018-07-06 22:37:03 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def get_all_prereqs():
-    req = []
-    if len(self.prereqs) == 0:
-        return []
-    else:
-        for course in req:
-            req.append(course.get_all_prereqs())
-        
